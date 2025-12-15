83c9466b3f94efa80c0f13a3bf19e13e9785d5c4
Wouter de Bruijn <wouter@hedium.nl>
2025-12-15 19:24:23 +0100

    ✨ Reintroduced display

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #define _TASK_SELF_DESTRUCT     // Enable tasks to "self-destruct" after disable
 
 #define RFID_ENABLE_PIN 14
-#define PUMP_PIN 21
+#define PUMP_PIN 15
 
 #include <Arduino.h>
 
@@ -13,6 +13,8 @@
 
 #include "rfid/RfidReader.h"
 #include "rfid/RfidStorage.h"
+
+#include "display/Display.h"
 
 #include "peripherals/Pump.h"
 
@@ -29,6 +31,7 @@ void enablePumpForDuration();
 HardwareSerial SerialRF(2);
 RfidReader rfidReader(&SerialRF, RFID_ENABLE_PIN, &rfidStorage, &enablePumpForDuration);
 Pump pump(PUMP_PIN);
+Display display;
 
 void pumpTimerCallback();
 
@@ -55,6 +58,19 @@ void rfidLoop()
 void displayLoop()
 {
   Serial.println(rfidReader.displayLine());
+  loopPMU(NULL);
+
+  display.clear();
+
+  // Shot machine display
+  display.setFont(u8g2_font_ncenB08_tr);
+  display.setCursor(0, 10);
+  display.print("Shot Machine");
+  display.setCursor(0, 30);
+  display.print("Last Tag:");
+  display.setCursor(0, 50);
+  display.print(rfidReader.displayLine());
+  display.sendBuffer();
 }
 
 Task displayTask(1000 * TASK_MILLISECOND, TASK_FOREVER, &displayLoop, &ts, true);
@@ -62,13 +78,15 @@ Task rfidTask(100 * TASK_MILLISECOND, TASK_FOREVER, &rfidLoop, &ts, true);
 
 void setup()
 {
-  setupBoards(true);
+  setupBoards();
   // When the power is turned on, a delay is required.
   delay(1500);
   setupLMIC(&rfidStorage);
 
   rfidReader.begin();
   pump.begin();
+  display.begin();
+  display.startupText();
 
   delay(1000);
 }
